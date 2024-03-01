547c61d8063f8e23dda57e2f14740168c0dd2240
hfxdanc <dcampbel.ca@gmail.com>
2024-02-29 20:58:28 -0400

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# winbox
-Flatpak build for Mikrotik WinBox utility
+# WinBox flatpak
+**!!! This wrapper is not verified by, affiliated with, sponsored or supported by MikroTīk in any way. !!!**
+
+Flatpak build for Mikrotik WinBox configuration utility.
+
+No binaries are included in the flatpak.  After agreement of terms the Windows X86_64 version of WinBox.exe along with the MikroTik logo will be downloaded and installed.
+
+WinBox is coded to only request the "default" Windows 7 Tahoma font it appears.  Wine (winehq runtime) provides a free alternative to the proprietary Microsoft typeface, but it seems to not provide proper kerning and text can be improperly spaced.  Font replacement handling in Wine will always use the original font if available, therefore the build removes the Wine provided Tahoma font.
+
+During the initial `flatpak run` a graphical window will display Tahoma alternatives that have been discovered and allow one to be selected.  If a selection has been made the registry key `HKEY_CURRENT_USER\Software\Wine\Fonts\Replacements`  will be updated appropriately.
+
+Windows 10 updated the "default" font from Tahoma to Segoe.  A git repository https://github.com/mrbvrz/segoe-ui-linux.git currently contains a copy. 
+
+The manifest will add any TrueType fonts that have been copied into the `./fonts` directory in the source tree into the resulting flatpak.
+
+To build the flatpak issue the following commands after cloning the repository ...
+
+`$ sh update.sh`
+
+Run the flatpak with ...
+
+`$ flatpak run org.flatpak.WinBox`
+
+The code installs Appstream metadata and a .desktop file so you can always execute the flatpak as a normal desktop  application using the Super key.
+
+If you are unsatisfied with the font you selected you can redo the installation phase with the command ...
+
+`$ flatpak run --command=/app/bin/winbox-fonts.sh org.flatpak.WinBox` 
+
+Alternatively you could remove the application's data to re-invoke the initial configuration script ...
+
+`$ rm -rf ~/.var/app/org.flatpak.WinBox/`
---
+++ update.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+rm -rf ./.flatpak-builder/ ./build-dir/
+
+DATE=$(date +%Y-%m-%d)
+VERSION=$(wget --spider --tries=1 https://mt.lv/winbox64 2>&1 | awk '/winbox64.exe$/ {split($0, a, /winbox/); print gensub(/\//, "", "g", a[2])}')
+
+cat << %E%O%T% >./WinBox.metainfo.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<component type="desktop-application">
+  <id>org.flatpak.WinBox</id>
+  <metadata_license>CC0-1.0</metadata_license>
+  <project_license>LicenseRef-proprietary</project_license>
+  <name>WinBox</name>
+  <summary>MikroTik RouterOS administration GUI</summary>
+  <developer_name>SIA Mikrotīkls</developer_name>
+  
+  <description>
+    <p>
+        Winbox is a small utility that allows the administration of MikroTik
+        RouterOS using a fast and simple GUI. It is a native Win32/Win64 binary
+        but can be run on Linux and macOS (OSX) using Wine. All Winbox
+        interface functions are as close as possible mirroring the console
+        functions, that is why there are no Winbox sections in the manual.
+        Some advanced and system critical configurations are not possible from
+        the Winbox, like MAC address change on an interface.
+    </p>
+    <p>This is a build of WinBox64 for Microsoft Windows, packaged for Linux as a Flatpak using Wine.</p>
+    <p>NOTE: This wrapper is not verified by, affiliated with, sponsored or supported by MikroTīk in any way.</p>
+  </description>
+  
+  <launchable type="desktop-id">org.flatpak.WinBox.desktop</launchable>
+
+  <url type="homepage">https://mikrotik.com</url>
+  <url type="help">https://help.mikrotik.com/docs/display/ROS/Winbox</url>
+
+  <screenshots>
+    <screenshot type="default">
+      <caption>WinBox Simple loader mode</caption>
+      <image>https://help.mikrotik.com/docs/download/attachments/328129/winbox_loader_simple_.png?version=1&amp;modificationDate=1570715</image>
+    </screenshot>
+    <screenshot>
+      <caption>Work Area and Child Windows</caption>
+      <image>https://help.mikrotik.com/docs/display/ROS/Winbox?preview=/328129/1409079/winbox3_work_area.png</image>
+    </screenshot>
+  </screenshots>
+
+  <releases>
+    <release version="$VERSION" date="$DATE"/>
+  </releases>
+
+  <content_rating type="oars-1.1"/>
+</component>
+%E%O%T%
+
+flatpak-builder --default-branch=stable \
+    --verbose \
+    --force-clean \
+    --install-deps-from=flathub \
+    --install \
+    --user \
+    ./build-dir ./org.flatpak.WinBox.yml
+
+# vi: set wrap:
